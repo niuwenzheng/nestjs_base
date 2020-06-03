fc378195646016e40b6082d3eb3a9bfb5a60556c
niuniu <861796052@qq.com>
2020-06-03 19:03:46 +0800

    暂时去除tool工具类

--- src/tools/util/index.ts
+++
@@ -1,7 +0,0 @@
-import { Utils } from './utils';
-const utils = new Utils();
-import { Cryptojs } from './cryptojs';
-const cryptojs = new Cryptojs();
-
-exports.utils = utils
-exports.cryptojs = cryptojs
