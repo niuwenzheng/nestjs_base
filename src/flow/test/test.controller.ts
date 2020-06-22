7a56bea29ab40ec5740f975e599a52481d4d6a72
niuniu <861796052@qq.com>
2020-06-22 16:44:15 +0800

    加入swagger文档插件包

--- src/flow/test/test.controller.ts
+++ src/flow/test/test.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { TestService } from './test.service';
+import { ApiProperty, ApiQuery } from '@nestjs/swagger'
 
 @Controller('test')
 export class TestController {
   constructor(private readonly testService: TestService) {}
 
+  // --------- 接口文档实列 STR ----------
+  @ApiProperty({ example: 'foo@example.com' })
+  @ApiQuery({ name: 'name', required: false })
+  // --------- 接口文档实列 END ----------
   @Get()
   async testTest(): Promise<any> {
     return await this.testService.testTest();
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 declare const module: any;
+
+// ------------- 文档插件 STR ---------------
+function createSwagger(app: INestApplication) {
+  const version = require('../package.json').version || '';
+  const options = new DocumentBuilder()
+    .setTitle('Nestjs 接口文档')
+    .setVersion(version)
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
+}
+// ------------- 文档插件 END ---------------
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ------------- 文档插件 STR ---------------
+  app.useGlobalPipes(new ValidationPipe());
+  if (process.env.SWAGGER_ENABLE && process.env.SWAGGER_ENABLE === 'true') {
+    createSwagger(app);
+  }
+  // ------------- 文档插件 END ---------------
+
   await app.listen(3000);
 
+  // ------------- 热重载 STR ---------------
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+  // ------------- 热重载 STR ---------------
 }
 bootstrap();
