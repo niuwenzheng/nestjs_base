6cdac65c2e7a9a8ab8d76f8704c9ad1c9c752657
niuniu <861796052@qq.com>
2020-06-23 18:34:53 +0800

    修改链接密码

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,12 +5,12 @@ import { IdService } from './id.service';
 @Global()
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://148.70.247.183:27017/app_server', {
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/app_server', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
 
-      user: 'nevin',
-      pass: '900301'
+      user: 'xxxx',
+      pass: 'xxxx'
     }),
     MongooseModule.forFeature([{ name: 'Id', schema: IdSchema }])
   ],
