7b1288aaf1afc76157d34ad8a488b0016e1519b4
niuniu <861796052@qq.com>
2020-08-11 19:31:59 +0800

    引入@types/mongoose，可指定表名

--- src/flow/users/schemas/users.schema.ts
+++ src/flow/users/schemas/users.schema.ts
@@ -1,14 +1,17 @@
 /*
  * @Author: niuwenzheng
  * @Date: 2020-04-03 16:47:03
- * @LastEditors: niuwenzheng
- * @LastEditTime: 2020-04-03 17:12:28
+ * @LastEditors: nevin
+ * @LastEditTime: 2020-08-11 19:30:40
  * @Description: file content
  */
 import * as mongoose from 'mongoose';
 
-export const UsersSchema = new mongoose.Schema({
-  user_id: String,
-  user_name: String,
-  age: Number,
-});
+export const UsersSchema = new mongoose.Schema(
+  {
+    user_id: String,
+    user_name: String,
+    age: Number,
+  },
+  { collection: 't_users', versionKey: false },
+);
